510610ea4e706e0b2672a96e8a494759635cee84
linluxx <karolina.kkv@gmail.com>
2022-08-20 14:34:35 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css'; // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
 console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
-galleryItems.forEach(item =>
-  galleryEl.insertAdjacentHTML(
-    'beforeend',
-    `<li class="gallery__item">
-    <a class="gallery__link" href='${item.original}'>
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+function createGalleryItems(images) {
+  return images
+    .map(
+      ({ original, preview, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href='${original}'>
     <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
+    class="gallery__img"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}" 
     />
-  </a>
-  </li>`
-  )
-);
+     </a>
+    </li>
+    `
+    )
+    .join('');
+}
 
 let galleryLightBox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',

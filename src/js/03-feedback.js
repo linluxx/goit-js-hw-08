50bb9ce841ed8925541ebc404fd297c993b972e5
linluxx <karolina.kkv@gmail.com>
2022-08-22 23:55:08 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,8 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
-const formData = {};
+let formData = {};
+// console.log(formData);
 const STORAGE_KEY = 'feedback-form-state';
 populateFeedback();
 
@@ -16,17 +17,22 @@ function onFormSubmit(evt) {
   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
 
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
+  // console.log(formData);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateFeedback() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedData) {
-    refs.email.value = savedData.email;
+  // console.log(savedData);
+  if (savedData.message) {
     refs.message.value = savedData.message;
+  }
+  if (savedData.email) {
+    refs.email.value = savedData.email;
   }
 }
